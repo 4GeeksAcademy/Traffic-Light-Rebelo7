902f43f06b1528f54f2a347739d203d9c4f1ba58
Rebelo7 <143350138+Rebelo7@users.noreply.github.com>
2023-11-12 18:39:23 +0000

    Add purple light

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
-import { ButtonCycle } from "./buttoncycle";
 
-const Home = () => {
+
+const Home = (props) => {
 	const [ selectedColor, setSelectedColor ] = useState ("green");
+	const [ show, setShow ] = useState (false);
 	return (
 		<div className="traffic-light">
 			<div onClick={() => setSelectedColor("red")} className={"light red"+(selectedColor === "red" ? " glow" : "")}></div>
 			<div onClick={() => setSelectedColor("yellow")} className={"light yellow"+(selectedColor === "yellow" ? " glow" : "")}></div>
-			<div onClick={() => setSelectedColor("green")} className={"light green"+(selectedColor === "green" ? " glow" : "")}></div>			
-		<ButtonCycle />
+			<div onClick={() => setSelectedColor("green")} className={"light green"+(selectedColor === "green" ? " glow" : "")}></div>
+			{ show && <div onClick={() => setSelectedColor("purple")} className={"light purple"+(selectedColor === "purple" ? " glow" : "")}></div>}
+			<button onClick={()=>setShow(!show)}>
+				{show === true ? "Hide" : "Purple Rain"}</button>		
 	</div>
 
 		
